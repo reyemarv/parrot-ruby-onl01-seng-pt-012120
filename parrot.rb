bb1142e22e81cdcc006f8d3c44d7e10d1d97ecda
Marvin Reyes <reyemarv@gmail.com>
2020-01-27 05:19:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(say = "Squawk!")
-  puts "#{say}"
+def parrot(phrase = "Squawk!")
+  puts "#{phrase}"
   "#{say}"
 end
